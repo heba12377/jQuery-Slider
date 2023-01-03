718749eddd9981d86a6ea35c6f6d1efa2682ff63
Heba El-Said <71638009+heba12377@users.noreply.github.com>
2023-01-03 16:43:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(".container").on("click", "img", function () {
+    var src = $(this).attr("src");
+    console.log(src);
+    $(".target1").attr("src", src);
+    $(".target1").attr("data-target", $(this).attr("id"));
+    $(".background").show();
+    $(".background").css("display", "flex");
+    $(".target").show();
+    //   $("body").addClass("change").attr("data-content", "bar");
+    //   $(".target").attr("display", "block");
+  });
+  
+  var imgsrc = [];
+  var images = $(".container img");
+  for (var i = 0; i < images.length; i++) {
+    imgsrc.push($(images[i]).attr("src"));
+  }
+  
+  $("button").on("click", function (e) {
+    var num = Number($(".target1").attr("data-target"));
+    if ($(this).attr("class") == "next") {
+      debugger;
+      if (num >= imgsrc.length - 1) {
+        $(".target1").attr("data-target", 0);
+        num = 0;
+      }
+      $(".target1").attr("data-target", num + 1);
+      $(".target1").attr("src", imgsrc[num + 1]);
+    } else {
+      if ($(".target1").attr("data-target") < 0) {
+        $(".target1").attr("src", imgsrc.length - 1);
+        num = imgsrc.length - 1;
+      }
+      $(".target1").attr("data-target", num - 1);
+      $(".target1").attr("src", imgsrc[num - 1]);
+    }
+  });
+  
+  $(".background").on("click", function (e) {
+    if (e.target == this) {
+      $(this).hide();
+      $(".target").hide();
+      console.log(this);
+      // $(".container").show();
+      // $(".target").hide();
+    }
+  });
+  $("body").css("opacity", "1");
+  
